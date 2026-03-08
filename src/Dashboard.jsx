1123710e0de9353cc48846b5ff7f2211408771a8
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-03-08 16:53:48 +0530

    updates done to code

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NotesContext } from './NotesContext'
 
-const dashboard = () => {
- 
- const [notes,setNotes]=useState(null)
- 
+const Dashboard = () => {
+
+    const [notes, setNotes] = useContext(NotesContext)
+
+    const handleSubmit = (e) => {
+        e.prevent.default()
+
+        if (!notes) {
+            alert("submit the notes")
+            return;
+        }
+        console.log(notes)
+        setNotes("")
+    }
     return (
-    <div>
-      <h2>dashboard</h2>  
-      <input type="text" placeholder='type a new note' value={notes}/>
-      <button onClick={handleSubmit}>add note</button>
-
-        
+        <div>
+            <h2>dashboard</h2>
+            <input type="text" placeholder='type a new note' value={notes} onChange={(e) = setNotes(e.target.value)}/>
+                <button onClick={handleSubmit}>add note</button>
         </div>
-  )
+
+
+    )
 }
 
-export default dashboard
+export default Dashboard;
